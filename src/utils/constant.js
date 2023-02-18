5e748c12b28b743a73ee1ce7856132f41e5e29d6
Nicolas Cardenas <nicoasp898@gmail.com>
2023-02-18 12:56:26 -0500

    commit fix

--- src/utils/constant.js
+++ src/utils/constant.js
@@ -1,3 +1,3 @@
 //export const API_HOST = "http://localhost:8080";
-export const API_HOST = "https://app-iishutyfhq-uc.a.run.app";
+export const API_HOST = "https://app-hafd5nl7ia-uc.a.run.app";
 export const TOKEN = "token";
